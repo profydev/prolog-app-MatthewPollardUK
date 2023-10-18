4d3f2441d721ebf99cb94db2b8dfdb153607d195
matthewpollardUK <matthewpoll@live.com>
2023-10-18 17:26:28 +0100

    add timeout for should

--- cypress/e2e/project-error.cy.ts
+++ cypress/e2e/project-error.cy.ts
@@ -1,15 +1,14 @@
 describe("Error Message Test", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/dashboard");
-
     cy.intercept("GET", "https://prolog-api.profy.dev/project", {
       statusCode: 500,
       response: "",
     }).as("getProjects");
+    cy.visit("http://localhost:3000/dashboard");
   });
 
   it("Displays an error message on API request failure", () => {
-    cy.wait(4000);
-    cy.get(".error-message").should("be.visible");
+    cy.wait("@getProjects");
+    cy.get(".error-message", { timeout: 10000 }).should("be.visible");
   });
 });
